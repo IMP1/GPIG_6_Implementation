02a5d5aac653847555534cb50bda50750a0cf628
IMP1 <huw_taylor@hotmail.co.uk>
2016-05-16 13:46:55 +0100

    fixed up tests (which now pass - yey)

--- network/src/network/Test.java
+++ network/src/network/Test.java
@@ -2,24 +2,26 @@ package network;
 
 import java.time.LocalDateTime;
 
+import network.StatusData.DroneState;
+
 @SuppressWarnings("unused")
 public class Test {
 
 	public static void main(String[] args) {
 		testMoveCommand();
-		testReportMessage();
+		testStatusMessage();
 		testScanMessage();
 		testPathMessage();
-		testC2();
 	}
 	
 	private static void testMoveCommand() {
-		String droneID = "1200afja8ahfafhf";
+		System.out.println("\tTEST: Move Command");
+		String id = "1200afja8ahfafhf";
 		LocalDateTime time = LocalDateTime.now();
 		double lat = 0.001252536;
 		double lon = 0.001252536;
 		double rad = 4.001252536;
-		MoveCommand c = new MoveCommand(droneID, time, lat, lon, rad);
+		MoveCommand c = new MoveCommand(id, time, lat, lon, rad);
 		
 		String message = c.toString();
 		System.out.println(message);
@@ -31,99 +33,120 @@ public class Test {
 		System.out.println(c2.longitude);
 		System.out.println(c2.radius);
 		
-		System.out.println("\n\n");
+		System.out.flush();
+		if (!id.equals(c2.id)) System.err.println("FALURE: different ids.");
+		if (!time.equals(c2.timestamp)) System.err.println("FALURE: different timestamps.");
+		if (lat != c2.latitude) System.err.println("FALURE: different latitude.");
+		if (lon != c2.longitude) System.err.println("FALURE: different longitude.");
+		if (rad != c2.radius) System.err.println("FALURE: different search radius.");
+		System.err.flush();
+		
+		System.out.println("\n");
 	}
 	
-	private static void testReportMessage() {
-		String status = "0001;2016-05-12T17:31:13.269;DATA;STATUS;0.001256475;0.87252587;100;FINE THANKS HOW ARE YOU?";
-		System.out.println(Message.getType(status) + "\n\n");
+	private static void testStatusMessage() {
+		System.out.println("\tTEST: Status Message");
+		String id = "567wgjsg8wtn2t";
+		LocalDateTime time = LocalDateTime.now();
+		double lat = 0.0074322;
+		double lon = 213.637;
+		double battery = 0.1134;
+		DroneState state = DroneState.MOVING;
+		double[] path = new double[] {
+			0.123483, 0.83564363,
+			0.6825725, 0.546164531,
+			0.654984651, 0.879564,
+		};
+		StatusData d = new StatusData(id, time, lat, lon, battery, state, path);
 		
-		StatusData r = new StatusData(status);
-		System.out.println(r.id);
-		System.out.println(r.timestamp);
-		System.out.println(r.latitude);
-		System.out.println(r.longitude);
-		System.out.println(r.batteryStatus);
-		System.out.println(r.status);
+		String message = d.toString();
+		System.out.println(message);
+		
+		StatusData d2 = new StatusData(message);
+		System.out.println(d2.id);
+		System.out.println(d2.timestamp);
+		System.out.println(d2.latitude);
+		System.out.println(d2.longitude);
+		System.out.println(d2.batteryStatus);
+		System.out.println(d2.status);
+		
+		System.out.flush();
+		if (!id.equals(d2.id)) System.err.println("FALURE: different ids.");
+		if (!time.equals(d2.timestamp)) System.err.println("FALURE: different timestamps.");
+		if (lat != d2.latitude) System.err.println("FALURE: different latitude.");
+		if (lon != d2.longitude) System.err.println("FALURE: different longitude.");
+		if (battery != d2.batteryStatus) System.err.println("FALURE: different battery level.");
+		if (!state.equals(d2.status)) System.err.println("FALURE: different drone status.");
+		System.err.flush();
+		
+		System.out.println("\n");
 	}
 	
 	private static void testScanMessage() {
-		String scanData = "some_id;2016-05-12T17:31:13.269;DATA;SCAN;0.0365364363;0.52636746;1337.101;1234.56789;0,1,2,3,4";
-		System.out.println(Message.getType(scanData) + "\n");
-		ScanData s = new ScanData(scanData);
-		System.out.println(s.id);
-		System.out.println(s.timestamp);
-		System.out.println(s.latitude);
-		System.out.println(s.longitude);
-		System.out.println(s.depth);
-		System.out.println(s.flowRate);
-		for (double distance : s.distanceReadings) {
+		System.out.println("\tTEST: Scan Message");
+		String id = "ghhuj2t8hjg4w4qwghse";
+		LocalDateTime time = LocalDateTime.now();
+		double lat = 0.0074322;
+		double lon = 213.637;
+		double depth = 12.56;
+		double flow = 0.00001;
+		double[] distances = new double[] {
+			0.01, 0.01, 0.01, 0.01, 0.02, 12.3456
+		};
+		ScanData s = new ScanData(id, time, lat, lon, depth, flow, distances);
+		
+		String message = s.toString();
+		System.out.println(message);
+		
+		ScanData s2 = new ScanData(message);
+		System.out.println(s2.id);
+		System.out.println(s2.timestamp);
+		System.out.println(s2.latitude);
+		System.out.println(s2.longitude);
+		System.out.println(s2.depth);
+		System.out.println(s2.flowRate);
+		for (double distance : s2.distanceReadings) {
 			System.out.print(distance + ", ");
 		}
+		
+		System.out.flush();
+		if (!id.equals(s2.id)) System.err.println("FALURE: different ids.");
+		if (!time.equals(s2.timestamp)) System.err.println("FALURE: different timestamps.");
+		if (lat != s2.latitude) System.err.println("FALURE: different latitude.");
+		if (lon != s2.longitude) System.err.println("FALURE: different longitude.");
+		if (depth != s2.depth) System.err.println("FALURE: different depth.");
+		if (flow != s2.flowRate) System.err.println("FALURE: different flow rate.");
+		if (distances.length != s2.distanceReadings.length) System.err.println("FALURE: different number of distance readings.");
+		for (int i = 0; i < Math.min(distances.length, s2.distanceReadings.length); i ++) {
+			if (distances[i] != s2.distanceReadings[i]) System.err.printf("FALURE: different distance reading #(%d).\n", i);
+		}
+		System.err.flush();
+		
+		System.out.println("\n");
 	}
 
 	private static void testPathMessage() {
-		// Data
-		final String id = "drone_id";
-		final LocalDateTime time = LocalDateTime.now();
-		final double[] points = new double[] { 
-			53.95717145394973 , -1.0783239204362758,
-			53.95725918465687 , -1.0784689578071749,
-			53.9573716884264  , -1.078658947616643 ,
-			53.957388824762155, -1.078682603210567 ,
-			53.95727725231522 , -1.078708680243239 ,
-			53.957098438376896, -1.0790372508549075,
-			53.956931545367794, -1.079042093732404 , 
-		};
-		// Sending
-		String message = "";
-		PathData pathObj = new PathData(id, time, points);
-		message = pathObj.toString();
-		System.out.printf("[-> '%s'\n", message);
+		System.out.println("\tTEST: Path Message");
+		String id = "67yth4wge9ew9";
+		LocalDateTime time = LocalDateTime.now();
+		double eta = 12;
+		PathData p = new PathData(id, time, eta);
 		
-		// Receiving
-		System.out.printf("[<- '%s'\n", message);
-		System.out.println(Message.getType(message) + "\n");
-		if (Message.getType(message) != PathData.class) System.err.println("FAILURE: Not the right message class.");
-		PathData pathObj2 = new PathData(message);
-		System.out.println(pathObj2.id);
-		System.out.println(pathObj2.timestamp);
-		System.out.println(pathObj2.points.length);
-		for (double point : pathObj2.points) {
-			System.out.print(point + ", ");
-		}
-		if (!pathObj2.id.equals(id)) System.err.println("FAILURE: Not the same drone id.");
-		if (!pathObj2.timestamp.equals(time)) System.err.println("FAILURE: Not the same timestamp.");
-		if (pathObj2.points.length != points.length) System.err.println("FAILURE: Not the same number of points.");
-		for (int i = 0; i < pathObj2.points.length; i ++) {
-			if (pathObj2.points[i] != points[i]) {
-				System.err.println("FAILURE: Point " + i + " is not the same. " + pathObj2.points[i] + " != " + points[i] + ".");
-			}
-		}
-	}
-	
-	private static void testC2() {
-		String message = "some_id;2016-05-12T17:31:13.269;DATA;SCAN;0.0102;0.2864;6.02;0.12;0,1,2,3,4";
+		String message = p.toString();
+		System.out.println(message);
 		
-		// if ( CLASS_WE_CARE_ABOUT.class.isAssignableFrom(Message.getType(MESSAGE_STRING)) ) { ... }
+		PathData p2 = new PathData(message);
+		System.out.println(p2.id);
+		System.out.println(p2.timestamp);
+		System.out.println(p2.eta);
 		
-		// void run() { ...
-		if (Data.class.isAssignableFrom(Message.getType(message))) {
-			// Handle Data Messages
-			if (Message.getType(message) == StatusData.class) {
-				StatusData report = new StatusData(message);
-				
-				// ... do whatever with status data ...
-				
-			} else if (MoveCommand.getType(message) == ScanData.class){
-				ScanData scandata = new ScanData(message);
-				
-				// ... do whatever with scan data ...
-				
-			}
-		} else if (Acknowledgement.class.isAssignableFrom(Message.getType(message))) {
-			// Handle Acknowledgement Messages
-		}
+		System.out.flush();
+		if (!p2.id.equals(id)) System.err.println("FAILURE: Not the same drone id.");
+		if (!p2.timestamp.equals(time)) System.err.println("FAILURE: Not the same timestamp.");
+		if (p2.eta != eta) System.err.println("FAILURE: Not the same number eta.");
+		System.err.flush();
+		
+		System.out.println("\n");
 	}
 	
 }
