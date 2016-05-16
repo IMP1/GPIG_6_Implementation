e1b88c3c9251d2e59d3c3b2d5ca14b38b82441d4
Alistair Cormack <cormackali@gmail.com>
2016-05-16 15:46:14 +0100

    Drone/C2 positions are now received from JSON (temp) Positions updated if markers available, otherwise new markers created

--- Mapbox_Testing/scripts/c2_backend.js
+++ Mapbox_Testing/scripts/c2_backend.js
@@ -1,37 +1,53 @@
 // JS Backend handles API calls
 
 var unitExamples = {
-	"C2":
+	"c2":
 	{"batteryLevel":100.0,
-	 "locLat":0.001256475,
-	 "locLong":0.87252587,
+	 "locLat":-1.08369,
+	 "locLong":53.959,
 	 "status":"FINE THANKS HOW ARE YOU?",
 	 "timestamp":{"date":{"year":2016,"month":5,"day":12},"time":{"hour":17,"minute":31,"second":13,"nano":269000000}}}
 	 
 	 ,"Drone 1":
 	{"batteryLevel":100.0,
-	 "locLat":0.001256475,
-	 "locLong":0.87252587,
+	 "locLat":-1.09024,
+	 "locLong":53.967,
 	 "status":"FINE THANKS HOW ARE YOU?",
 	 "timestamp":{"date":{"year":2016,"month":5,"day":12},"time":{"hour":17,"minute":31,"second":13,"nano":269000000}}}
 	 
 	 ,"Drone 2":
 	{"batteryLevel":100.0,
-	 "locLat":0.001256475,
-	 "locLong":0.87252587,
+	 "locLat":-1.080262,
+	 "locLong":53.967,
 	 "status":"FINE THANKS HOW ARE YOU?",
 	 "timestamp":{"date":{"year":2016,"month":5,"day":12},"time":{"hour":17,"minute":31,"second":13,"nano":269000000}}}
 	 
 	 ,"Drone 3":
 	{"batteryLevel":100.0,
-	 "locLat":0.001256475,
-	 "locLong":0.87252587,
+	 "locLat":-1.086676,
+	 "locLong":53.963,
 	 "status":"FINE THANKS HOW ARE YOU?",
 	 "timestamp":{"date":{"year":2016,"month":5,"day":12},"time":{"hour":17,"minute":31,"second":13,"nano":269000000}}}
 	 
 	 
 }
 
+var units = [];
+
+function addNewUnit(id, symbol, coordinates, batteryLevel, status, lastUpdated){
+	
+	// Map symbol is generated based on ID
+	console.log(id)
+	if(id == "c2"){
+		symbol = "castle";
+	}else{
+		symbol = "marker";
+	}
+	
+    var unit = new Unit(id, symbol, coordinates, batteryLevel, status, lastUpdated);
+    units.push(unit);
+	return unit;    
+}
 
 function getUnitsInfo(){
 	
@@ -44,18 +60,36 @@ function getUnitsInfo(){
 	Object.keys(unitsJSON).forEach(function (unitKey) {
 	   
 	   var unitJSON = unitsJSON[unitKey];
-	   console.log(unitJSON);
 	   
+	   // Check if Unit already exists
+	   var unit;
+	   units.forEach(function(existingUnit) {
+		   if(existingUnit.id == unitKey){
+			   unit = existingUnit;
+		   }
+	   }, this);
 	   
+	   if(unit){
+		   // If Unit Exists Update It
+		   updateUnitFromJSON(unit, unitKey, unitJSON);
+	   }else{
+		   // Else Create a new one
+		   var coordinates = [unitJSON.locLat, unitJSON.locLong];
+    	   unit = addNewUnit(unitKey, 'marker', coordinates, unitJSON.batteryLevel, unitJSON.status, unitJSON.timestamp);
+		   var marker = addNewUnitMarker(unit);
+		   unit.marker = marker;
+		   console.log(unit);
+	   }
+	   
+	   // TODO : Remove Units if they no longer exist
 	   
 	});
 	
-	console.log(markers);
-	
-	
+	map.getSource('markers').setData(markers);
 	
 }
 
 function setupAPICalls(){
-	setInterval(getUnitsInfo, 1000);
+	setInterval(function(){getUnitsInfo()}, 1000);
+	map.repaint = true;
 }
